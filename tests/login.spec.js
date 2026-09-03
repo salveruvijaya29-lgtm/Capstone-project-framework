8bb01fecc41e2fb3d0b21113073a62144f83eb1c
Vijaya <vijaya.salveru@wnco.com>
2026-09-03 10:39:12 +0530

    login

---
+++ tests/login.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+
+test('successful login', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/login');
+
+  await page.locator('#username').fill(process.env.USERNAME);
+  await page.locator('#password').fill(process.env.PASSWORD);
+  await page.locator('#submit-login').click();
+
+  await expect(page).toHaveURL(/secure/);
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+});
